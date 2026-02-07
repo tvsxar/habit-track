cfdc93161dbeec309e769cbacf14f791beb8f070
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-02-07 18:01:28 +0200

    init: setup backend project with dependencies

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 1011;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend HabitTrack works!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
