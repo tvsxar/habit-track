e0ce530e0928b61115891fa0cb5fd38fc3694d09
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-02-08 22:36:20 +0200

    chore: add MongoDB connection setup

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.once("open", () => {
+      console.log("✅ Connected to:", mongoose.connection.name);
+    });
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
