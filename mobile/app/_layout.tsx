9d727b9fdad63ebaa426b3700e30d2087e5019d6
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-02-25 12:57:55 +0200

    feat: created AuthContext and useAuth hook for user authentication, wrapped RootLayout with AuthProvider

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,11 +1,32 @@
 import { Stack } from "expo-router";
 import { HabitProvider } from '../hooks/useHabits';
+import { AuthProvider, useAuth } from '../hooks/useAuth';
+import LoadingScreen from "@/components/LoadingScreen";
+import { useTheme } from "@/hooks/useTheme";
+
+function RootStack() {
+  const { user, loading } = useAuth();
+  const { theme } = useTheme();
+
+  if (loading) return <LoadingScreen theme={theme} />;
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      {user ? (
+        <Stack.Screen name="(tabs)" />
+      ) : (
+        <Stack.Screen name="(auth)/login" />
+      )}
+    </Stack>
+  );
+}
 
 export default function RootLayout() {
   return (
-    <HabitProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </HabitProvider>);
+    <AuthProvider>
+      <HabitProvider>
+        <RootStack />
+      </HabitProvider>
+    </AuthProvider>
+  );
 }
