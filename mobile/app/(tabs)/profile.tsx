5623bfabf1619e008f88d2862013db3a14f4acde
Taras Poiatsyka <tarasofficial82@gmail.com>
2026-03-03 09:30:35 +0200

    feat(profile-screen): add dynamic habits count and max streak calculation

--- mobile/app/(tabs)/profile.tsx
+++ mobile/app/(tabs)/profile.tsx
@@ -8,12 +8,20 @@ import { useTheme } from "../../hooks/useTheme";
 import { createProfileStyles } from "../../styles/profileStyles";
 import { useAuth } from "@/hooks/useAuth";
 import { useRouter } from "expo-router";
+import { useHabits } from "../../hooks/useHabits";
 
 export default function Profile() {
   const { theme, toggleTheme, isDark } = useTheme();
   const styles = createProfileStyles(theme);
   const { logout, user } = useAuth();
   const router = useRouter();
+  const { habits } = useHabits();
+
+  const habitsCount = habits.length;
+
+  const maxStreak = habits.reduce((max, habit) => {
+    return habit.streak > max ? habit.streak : max;
+  }, 0);
 
   const handleLogout = async () => {
     await logout();
@@ -27,12 +35,12 @@ export default function Profile() {
 
       <View style={styles.statsContainer}>
         <View style={styles.statCard}>
-          <Text style={styles.statNumber}>5</Text>
+          <Text style={styles.statNumber}>{habitsCount}</Text>
           <Text style={styles.statLabel}>Habits</Text>
         </View>
 
         <View style={styles.statCard}>
-          <Text style={styles.statNumber}>12 🔥</Text>
+          <Text style={styles.statNumber}>{maxStreak} 🔥</Text>
           <Text style={styles.statLabel}>Current Streak</Text>
         </View>
       </View>
